f6590191b133a07ef5ab17a5e9163ba8e39eb41f
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2011-12-17 14:37:19 -0500

    Better description for method & commit

--- lib/generators/hashrocket/install/install_generator.rb
+++ lib/generators/hashrocket/install/install_generator.rb
@@ -4,10 +4,10 @@ module Hashrocket
   class InstallGenerator < ::Rails::Generators::Base
     include Generators
 
-    def convert_layout
+    def setup_layout
       template 'application.html.haml', 'app/views/layouts/application.html.haml'
       remove_file 'app/views/layouts/application.html.erb'
-      commit 'Convert layout to haml'
+      commit 'Setup base layout'
     end
 
   end
